6cf07200e0bc95d445694318a5c4a4eef1b2c19a
stewartweiss <sweiss@hunter.cuny.edu>
2019-03-08 10:44:23 -0500

    added rubric

---
+++ blog_activity.php
@@ -0,0 +1,101 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+ 
+<head>
+<meta name="revisit-after" content="14 days">
+<meta name="keywords" content="Stewart Weiss, Hunter College, computer science, open source, open source software">
+<meta name="description" content="Stewart Weiss, PHD Courant Institute, Professor Hunter College"> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="../../styles/styles.css" rel="stylesheet" type="text/css">   
+</head>
+
+
+<body>
+<?php require("../../menu.html"); ?>
+
+<div id="main_body" style="width:100%">
+
+<h2 id="weekly-journalblog">Weekly Journal/Blog</h2>
+<p>Every student will maintain a journal for this class, with entries made on a weekly basis. Writing a journal is a way to improve your communication skills. The journal also serves as a record of what you have accomplished in this class. We will use a <em>blog</em> as the medium for these journal entries.</p>
+<p>You will be given a <strong>public repository</strong> for this course-related blog. The fact that it is public implies that anyone in the world can read what is put into this blog. The exact instructions for how to set up the blog are in the <code>README.md</code> file in the blog repository on GitHub that I have created for you. The blog template is in the class organization at</p>
+<p><code>https://github.com/hunter-college-ossd-spr19/weekly</code></p>
+<p>Your individual blog is named</p>
+<p><code>https://github.com/hunter-college-ossd-spr19/gh-username-weekly</code></p>
+<p>where <code>gh-username</code> is your GitHub account name. The blog repository is set up in such a way that your blog serves as the source code for a web-page hosted on GitHub, using <code>GitHub Pages</code>. For information about <code>GitHub Pages</code>, see <a href="https://help.github.com/articles/what-is-github-pages/">What is GitHub Pages</a>. Because <code>GitHub Pages</code> has been activated for this repository, you will automatically have a webpage at the URL</p>
+<p><code>https://hunter-college-ossd-spr19.github.io/gh-username-weekly/</code></p>
+<p>where <code>gh-username</code> is your GitHub account name.</p>
+<p>Your blog entries must be written using <strong>Markdown</strong>. It is very important that you make them as professional-looking as possible, and using Markdown will make this possible. Because the blog files are hosted on GitHub, you can use either the standard Markdown syntax or <strong>GitHub-Flavored Markdown</strong>. Help with Markdown can be found at the following sites:</p>
+<ul>
+<li><a href="https://www.markdowntutorial.com/">Markdown Tutorial</a></li>
+<li><a href="https://guides.github.com/features/mastering-markdown/">Mastering Markdown (GitHub)</a></li>
+<li><a href="https://www.markdownguide.org/cheat-sheet">Markdown Guide</a></li>
+<li><a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">Markdown Cheat Sheet</a></li>
+</ul>
+<p><strong>Each week, by Thursday night, a new post should be added to the blog for the week ending that day.</strong> For example, the first day of classes is Friday, January 25, so the first week is from January 25 through January 31.</p>
+<p>There is directory in your repository</p>
+<p><code>https://github.com/hunter-college-ossd-spr19/gh-username-weekly</code></p>
+<p>named <code>_posts</code>. This directory was filled with three files initially, named</p>
+<ul>
+<li><code>2019-02-01-week01.md</code></li>
+<li><code>2019-02-08-week02.md</code></li>
+<li><code>2019-02-15-week03.md</code></li>
+</ul>
+<p>respectively. The blog post for the first week is created by editing the first file, for the second week, the second file, and so on. You should create new files as the semester advances, using the exact same form for their names.</p>
+<p>The blog post for a given week should be based on the following conditions:</p>
+<ul>
+<li><p>If an assignment was given that specifically asked for something to be written in the blog for the week, then the post for the week should include whatever<br />
+was requested in the assignment.</li>
+<li><p>Whether or not anything was assigned, the post for that week should include a summary of all course-related activity.</li>
+<li><p>If you have made any contributions of any kind, whether they are to <em>OpenStreetMap</em>, <em>Wikipedia</em>, or any software project, the post should include a list of those contributions.</li>
+</ul>
+
+<h3 id="a-rubric-for-assessment-of-blog-posts">A Rubric for Assessment of Blog Posts</h3>
+<p>Blog posts are a component of your grade in the course. I have never found a good way to assign a numerical score to narrative writing, like a blog post. Nonetheless, these posts must be assessed. Therefore, I am using the following <em>ordinal</em> scale for assessing the posts. &quot;<em>Ordinal</em>&quot; means that it is linear but that the numbers have no arithmetic properties other than one being larger or smaller than another. A score of 3 is not three times as good as a score of 1.</p>
+<table id="t01">
+<thead>
+<tr class="header">
+<th align="right">Grade</th>
+<th align="left">Assessment</th>
+</tr>
+</thead>
+<tbody>
+
+<tr class="odd">
+<td align="right" style="font-size:medium">3</td>
+<td align="left"  style="font-size:medium">The post is thoughtful, well-written, well-formatted, satisfies the stated objectives for the posts for the given week, and demonstrates a significant amount of effort.</td>
+</tr>
+<tr class="even">
+<td align="right" style="font-size:medium" >2</td>
+<td align="left"  style="font-size:medium" >The post is not particularly thoughtful, adequately-written, adequately-formatted, and satisfies the stated objectives for the posts for the given week, and demonstrates a modest amount of effort.</td>
+</tr>
+<tr class="odd">
+<td align="right" style="font-size:medium">1</td>
+<td align="left"  style="font-size:medium">The post satisfies the stated objectives for the posts for the given week, but not much more. There is little thought given to it, and almost no effort.</td>
+</tr>
+<tr class="even">
+<td align="right" style="font-size:medium">0</td>
+<td align="left"  style="font-size:medium">The post was not written.</td>
+</tr>
+</tbody>
+</table>
+
+<!-- include navigation bar, footer, and statcounter -->
+<?php include_once('../../pagebottom.php'); ?> 
+
+
+<a href="http://jigsaw.w3.org/css-validator/check/referer">
+    <img style="border:0;width:88px;height:31px"
+        src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+        alt="Valid CSS!" >
+</a>
+
+<a href="http://validator.w3.org/check?uri=referer">
+    <img src="http://www.w3.org/Icons/valid-html401-blue"
+        alt="Valid HTML 4.01 Transitional" style="border:0;width:88px;height:31px">
+</a>
+
+</body>
+</html>
